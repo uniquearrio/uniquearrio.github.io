83d57a987536edddbe49d5b8c61c84d94b560de4
uniquearrio <94905862+uniquearrio@users.noreply.github.com>
2022-06-13 11:25:32 +0630

    Delete Level-3/vue/custom-app directory

--- Level-3/vue/custom-app/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import Bootstrap from 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-
-createApp(App).use(router,Bootstrap).mount('#app')
